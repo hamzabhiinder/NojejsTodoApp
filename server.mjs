8d8192c63a258b954a8df14c728be8dfe99ad060
unknown <hamzabhinder5@gmail.com>
2022-10-17 13:23:12 +0500

    Full Todo App

--- server.mjs
+++ server.mjs
@@ -1,11 +1,9 @@
-import express from 'express';
-import cors from 'cors'
-import mongoose from "mongoose";
 
-
+import express from "express";
+import mongoose, { Mongoose } from "mongoose";
+import cors from "cors";
 const app = express()
 const port = 3000
-
 
 app.use(cors())
 app.use(express.json())
@@ -13,100 +11,154 @@ app.use(express.json())
 
 
 
+app.get('/todos', (req, res) => {
+    todoModel.find({}, (error, saved) => {
+        if (!error) {
 
-
-////////////////Post///////////////////////////////////////////////
-
+            res.send({
+                message: "Your Data is Get Successfully",
+                data: saved
+            })
+        } else {
+            res.status(500).send({
+                message: "Server Error"
+            })
+        }
+    })
+})
 app.post('/todos', (req, res) => {
 
-    todoModel.create({ text: req.body.text }, (err, saved) => {
-        if (!err) {
-            console.log(saved)
-            res.send({
-                message: 'Your todo is Saved'
-            })
+    todoModel.create({ text: req.body.text }, (error, saved) => {
+        if (!error) {
 
+            res.send({
+                message: "Your Data is Posted Successfully"
+            })
         } else {
             res.status(500).send({
                 message: "Server Error"
             })
-
         }
     })
 
 })
 
 
-////////////////GET///////////////////////////////////////////////
+// app.delete('/todos', (req, res) => {
+//     todoModel.deleteMany({}, (error, saved) => {
+//         if (!error) {
 
-app.get('/todos', (req, res) => {
+//             res.send({
+//                 message: "Your Data is Deleted Successfully",
+//                 data: saved
+//             })
+//         } else {
+//             res.status(500).send({
+//                 message: "Server Error"
+//             })
+//         }
+//     })
+// })
 
-    todoModel.find({}, (err, docs) => {
-        if (!err) {
-            console.log(docs)
-            res.send({
-                message: 'here is Your todo',
-                data:docs
-            })
+app.delete('/todos', async (req, res) => {
+   
 
-        } else {
-            res.status(500).send({
-                message: "Server Error"
-            })
-
-        }
-    })
-
+    try {
+        let data = await todoModel.deleteMany({}, { text: req.body.text }).exec()
+        res.send({
+            message: `Your is Deleted Successfully`,
+            data:data
+           
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: "Server Error"
+        })
+    }
 })
-
-
-app.delete('/todos/:id', (req, res) => {
-    let id = req.params.id
-
-    todoModel.findByIdAndDelete(id, (err, docs) => {
-        if (!err) {
-            console.log(docs)
-            res.send({
-                message: 'here is Delete todo',
-               
-            })
-
-        } else {
-            res.status(500).send({
-                message: "Server Error"
-            })
-
-        }
-    })
-
-})
-
 
 // app.delete('/todos/:id', (req, res) => {
 //     let id = req.params.id
-//     console.log(id)
-//     console.log(id);
-//     todoModel.findByIdAndDelete(id, (err, data) => {
-//         res.send({
-//             data: data
-//         })
+//     todoModel.findByIdAndDelete(id, (error, saved) => {
+//         if (!error) {
+
+//             res.send({
+//                 message: `Your ${id} is Deleted Successfully`,
+
+//             })
+//         } else {
+//             res.status(500).send({
+//                 message: "Server Error"
+//             })
+//         }
 //     })
 // })
 
 
+app.delete('/todo/:id', async (req, res) => {
+    let id = req.params.id
+
+    try {
+        let data = await todoModel.findByIdAndDelete(id, { text: req.body.text }).exec()
+        res.send({
+            message: `Your ${id} is Deleted Successfully2`,
+           
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: "Server Error"
+        })
+    }
+})
+//////Updated
+app.put('/todo/:id', async (req, res) => {
+    let id = req.params.id
+
+    try {
+        let data = await todoModel.findByIdAndUpdate(id, { text: req.body.text }, { new: true }).exec()
+        res.send({
+            message: `Your ${id} is Updated Successfully2`,
+            data: data
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: "Server Error"
+        })
+    }
+})
+// app.put('/todos/:id',(req,res)=>{
+//     let id=req.params.id
+//     todoModel.findByIdAndUpdate(id,{text:req.body.text},(error,saved)=>{
+//         if (!error) {
+
+//             res.send({
+//                 message :`Your ${id} is Updated Successfully`,
+
+//             })
+//         } else {
+//             res.status(500).send({
+//                 message:"Server Error"
+//             })
+//         }
+//     })
+// })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-////////////////Schemas///////////////////////////////////////////////
 
-let todoSchema = new mongoose.Schema({
+
+////////////////////////Schema////////////////////////////////////////////////////
+
+let todoSchema = mongoose.Schema({
     text: { type: String, required: true },
-    classId: String,
+    classId: { type: String },
     Date: { type: Date, default: Date.now }
 })
 
 let todoModel = mongoose.model('todos', todoSchema);
+
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 let dbURI = "mongodb+srv://AMEER:882292@cluster0.boegp.mongodb.net/abcdatabase?retryWrites=true&w=majority";
@@ -134,8 +186,3 @@ process.on('SIGINT', () => {
 });
 
 ////////////////mongodb connected disconnected events///////////////////////////////////////////////
-
-
-
-
-
